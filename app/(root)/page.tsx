fa9eaa463cea795b1c32dce6fb580343716b585a
Atheeth1 <atheeth.iccc@gmail.com>
2024-03-29 13:22:27 +0530

    Add to remote git hub

---
+++ app/(root)/page.tsx
@@ -0,0 +1,17 @@
+import Collections from "@/components/Collections";
+import ProductList from "@/components/ProductList";
+
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <Image src="/banner.png" alt="banner" width={500} height={100} className="w-screen" />
+      <Collections />
+      <ProductList />
+    </>
+  );
+}
+
+export const dynamic = "force-dynamic";
+
